7c4c7710f262d75fb43859cc25dc78d53c5cadca
GtiWebApp <info@GtiWebApp.com>
2023-04-15 21:28:10 -0400

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,4 @@
+const Footer = () => {
+  return <div>Footer Informations</div>;
+};
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,4 @@
+const Navbar = () => {
+  return <div>This is the navbar components</div>;
+};
+export default Navbar;
